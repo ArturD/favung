6fe9f5fd2a5f3cd4d655a7620ff9dc61e1acb7d5
Artur Dwornik <Artur.Dwornik@gmail.com>
2011-11-18 14:47:00 +0100

    Update: Rails version to ~> 3.1.2

--- webfront/config/environments/development.rb
+++ webfront/config/environments/development.rb
@@ -11,7 +11,7 @@ Favung::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -22,6 +22,8 @@ Favung::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.assets.enabled = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
