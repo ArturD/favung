b381e4c138e55851aeacf1a3cc5f732be8da1503
Artur Dwornik <Artur.Dwornik@gmail.com>
2011-11-24 18:43:16 +0100

    fix ulimit usage

--- agent/compilers/executor.rb
+++ agent/compilers/executor.rb
@@ -9,7 +9,8 @@ class Executor
 
   def run(in_file, out_file)
     $logger.info "exec #{@cmd} < #{in_file} > #{out_file} 2> /dev/null"
-    `ulimit -t 2 #{@cmd} < #{in_file} > #{out_file} 2> /dev/null`
+    `ulimit -t 2; #{@cmd} < #{in_file} > #{out_file} 2> /dev/null`
+    $logger.info "exit status: #{$?.exitstatus}"
     return false, false, true if $?.exitstatus != 0
     return false, false, false
   end
