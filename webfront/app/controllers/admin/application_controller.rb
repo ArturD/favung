5efc4297af2f03372a334c868887d0f0d93b088e
Artur Dwornik <Artur.Dwornik@gmail.com>
2011-11-26 16:02:04 +0100

    change admin controller security exception to redirection

--- webfront/app/controllers/admin/application_controller.rb
+++ webfront/app/controllers/admin/application_controller.rb
@@ -1,10 +1,12 @@
 class Admin::ApplicationController < ApplicationController
   before_filter :is_admin?
+  layout "admin"
 
   private
   def is_admin?
     unless current_user && current_user.admin?
-      raise "Access Denied"
+      flash[:error] = "You must have admin privileges to access admin panel"
+      redirect_to new_session_path :user
     end
   end
 end
