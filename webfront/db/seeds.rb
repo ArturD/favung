6648a8681b82557f561de07dd726a20ad44d14bc
Artur Dwornik <Artur.Dwornik@gmail.com>
2011-11-26 16:03:15 +0100

    add seeders for default user and admin. use rake db:seed. Dont forget to change password afterwards

--- webfront/db/seeds.rb
+++ webfront/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+if User.where(:role => "admin").count == 0
+  puts "== creating new admin (admin@favung.com , pass: changeme) =="
+  puts " !!! CHANGE PASSWORD FOR THIS USER !!!  "
+  user = User.new :email => "admin@favung.com", :password => "changeme"
+  user.role = "admin"
+  user.save!
+else 
+  puts "== admin exists =="
+end
+if User.where(:role => "user").count == 0
+  puts "== creating new user (user@favung.com, pass: changeme) =="
+  puts " !!! CHANGE PASSWORD FOR THIS USER !!!  "
+  User.create! :email => "user@favung.com", :password => "changeme"
+else 
+  puts "== user exists =="
+end
+
+
